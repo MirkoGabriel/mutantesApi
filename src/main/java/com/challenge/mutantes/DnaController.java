c6e8c48bcd7f37c71e18894d8607d67c63eaa42f
mirkogab98 <mirkogabriel67@gmail.com>
2020-11-08 20:29:22 -0300

    version 2

--- src/main/java/com/challenge/mutantes/DnaController.java
+++ src/main/java/com/challenge/mutantes/DnaController.java
@@ -1,5 +1,7 @@
 package com.challenge.mutantes;
-
+/*
+ * @mirko
+ */
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
